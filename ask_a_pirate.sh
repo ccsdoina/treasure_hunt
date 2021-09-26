8520b3a51742086b71a04340c68a4a9120bbc4a7
Robin Engler <robin.engler@sib.swiss>
2021-09-26 22:23:33 +0200

    Fishermen market

---
+++ ask_a_pirate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source .script/quest_functions.sh
+
+# Quest starts here...
+pirate_say "Welcome to Oyster bay!\n"
+sleep 1
+
+echo "Are you looking to buy some fish?"
+sleep 2
+
+echo "Blimey.. a ship! That's some serious asking there... let me see..."
+sleep 3
+
+echo "I heard the Buccaneers have one for sale, you might want to try your luck there."
+sleep 3
+echo "You're sure you don't need any fresh fish?"
+
+
+exit 0
